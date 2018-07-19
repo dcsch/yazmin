5d2fa90f0d6015ec394b250fb49d59756f536794
David Schweinsberg <david.schweinsberg@gmail.com>
2018-07-18 19:30:20 -0700

    Updated Xcode settings

--- YazminTests/YazminTests.h
+++ YazminTests/YazminTests.h
@@ -3,7 +3,7 @@
 //  YazminTests
 //
 //  Created by David Schweinsberg on 17/03/11.
-//  Copyright 2011 Synchroma Pty Ltd. All rights reserved.
+//  Copyright 2011 David Schweinsberg. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
